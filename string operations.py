8a2c33d4665b11b6a5be16306c4d0f7e1b0e20b0
dennyleo <dennisomoding3@gmail.com>
2019-10-15 20:46:14 +0300

    string

---
+++ string operations.py
@@ -0,0 +1,38 @@
+#concatenation
+first_name="dennis"
+last_name="omoding"
+#use .format to concatenate
+#full_name="{}{}".format(first_name,last_name)
+#print(full_name
+print("the tallest person is {} from the family of {}".format(first_name,last_name))
+#full_name=first_name+" "+last_name
+#print(full_name)
+
+names="dennis omoding"
+name2="brian bett"
+name3="brigid bett"
+print(names.capitalize())
+print(name2.title())
+print(name3.upper())
+
+sen="i am going to have dinner because dinner makes me stronger"
+#count displays the number of times a string appears
+print(sen.count("dinner"))
+#count number of letter a
+print(sen.count("a"))
+#count number of spaces
+print(sen.count(" "))
+#strng slicing
+#left of the colon signifies the starting point whereas the right of the colon signifies the letter before the last letter
+print(sen[0])
+# sen[0] will print the first letter of sen sen[-1] will print the display the letter before the last
+print(sen[0:-1])
+print(sen[0:9])
+jina="omoding"
+print(jina[0:-1])
+print(jina[1:6])
+
+#revert jina
+print(jina[::-1])
+print(jina.split()) #splits the string
+print(sen.split())
--- variables.py
+++ variables.py
@@ -22,10 +22,48 @@ print("the var containing",d,"is",type(d))
 print("the var contatining",f,"is",type(f))
 
 #data types
-# integer
-# float
-# string
+# integer is a data type that holds whole numbers
+number_of_students=45
+math=75
+type_of_math=type(math)
+print(type_of_math)
+#or
+print(type(math))
+
+# float a number with decimal point
+opening_balance=100.0
+print(type(opening_balance))
+
 # boolean
+g=True
+h=False
+print(type(g))
+
+#string
+#any character is a string
+
+name="dennis omoding"
+print(type(name))
+id="31699319"
+print(type(id))
+#type casting-converting from one data type to another
+id=int(id)
+#strings are enclosed in either double quotes or single quotes or tripple quotes for several lines
+
+sentence=("imma call you right back")
+#or
+sentence=('imma call you right back')
+print(sentence)
+#use double quotes if the contents contain apostrophe
+#triple quotes
+
+paragraph="""
+imma call you back
+when you wake up
+and raise the issue
+"""
+print(paragraph)
+
 # list
 # tuple
 # dictionary
