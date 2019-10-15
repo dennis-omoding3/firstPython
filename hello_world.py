b4a217a686081df1c35d6c0ec507c88a66aa4bce
dennyleo <dennisomoding3@gmail.com>
2019-10-15 13:58:20 +0300

    pythonproject

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#the line below will print "hello world" to the console
+print("hello world")
+# the line below
+# calculates total of three numbers
+# and store the result to variable total
+total=20+20+30
+
+print(total)
+
+sales="75-30+25"
+print(sales)
---
+++ variables.py
@@ -0,0 +1,22 @@
+#variables are containers for storing daa values for reference
+
+#thus is normally primary memory(RAM)
+total = 45
+
+dennis="tall"
+
+student=16
+
+print(dennis)
+a=20
+b="great"
+c=5.813
+d=3+2j
+f=4e7
+
+
+print("the variable type for",a,"is", type(a))
+print("the variable containing",b, "is",type(b))
+print("the variable containing",c,"is",type(c))
+print("the var containing",d,"is",type(d))
+print("the var contatining",f,"is",type(f))
